768e78fb3423c5a75cb5d071dba76bc4c81a2359
Vlad <vladosadcyj340@gmail.com>
2023-10-10 19:20:35 +0300

    Change some tasks in Lection 1 and add Task 5 into Lection 2

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,5 +1,4 @@
-# Вибір напрямку переводу
-your_choice = input("Choose what do you want (C->F or F->C) Print C (if 1) or F (if 2): ")
+your_choice = input("Print C - if you want convert celsius to fahrenheit, or F if vice versa: ")
 if your_choice == "C":
     your_number = float(input("Enter the temperature in degrees Celsius: "))
     fahrenheit = (your_number * 9/5) + 32
@@ -8,4 +7,6 @@ elif your_choice == "F":
     your_number= float(input("Enter the temperature in degrees Fahrenheit: "))
     celsius = (your_number - 32) * 5/9
     print(f"{your_number} degrees Fahrenheit= {celsius} degrees Celsius")
+else:
+    print("Sorry!You write not correct letter")
 
---
+++ Lection 2/Task 5/main.py
@@ -0,0 +1,12 @@
+your_list=(input("Enter your message: "))
+key=int(input("Enter your key: "))
+result=''
+main_alfavit='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+for i in your_list:
+    pos=main_alfavit.find(i)
+    newpos=pos+key
+    if i in main_alfavit:
+        result+=main_alfavit[newpos]
+    else:
+        result+=i
+print("=======================", '\n', result)
