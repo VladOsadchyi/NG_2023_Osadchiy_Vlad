a5bfbedac0dcab426ae852e885e9b512e82bc730
Vlad <vladosadcyj340@gmail.com>
2023-11-08 08:54:32 +0200

    Changes Task 3 in Lection 3

--- Lection 3/Task 3/main.py
+++ Lection 3/Task 3/main.py
@@ -1,18 +1,25 @@
 import json
-file_name = input("Enter the name of your JSON file or the way to it: ")
-with open(file_name, 'r') as file:
-        data = json.load(file)
-your_key = input("Enter the name of key: ")
-stack = [data]
-found=None
-while stack:
+def find_key(data, target_key):
+    stack = [data]
+    found = None
+    while stack:
         current_data = stack.pop()
         if isinstance(current_data, dict):
             for key, val in current_data.items():
-                    if key == your_key:
-                        found = val
-                        break
-                    if isinstance(val, dict):
-                        stack.append(val)
-print(f"The value of key '{your_key}': {found}")
+                if key == target_key:
+                    found = val
+                    break
+                if isinstance(val, dict):
+                    stack.append(val)
+    return found
+file_name = input("Enter the name of your JSON file or the path to it: ")
+with open(file_name, 'r') as file:
+    data = json.load(file)
+your_key = input("Enter the name of the key you want to find: ")
+result = find_key(data, your_key)
+if result is not None:
+    print(f"The value of key '{your_key}': {result}")
+else:
+    print(f"Key '{your_key}' not found in the JSON data.")
+
         
